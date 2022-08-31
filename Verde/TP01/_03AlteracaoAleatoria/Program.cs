ebbb4e70a3af5817d580e2a3b1509aab0461986a
João Pedro Anicio Botelho <1382123@sga.pucminas.br>
2022-08-31 20:38:03 -0300

    Atividades Lab

--- Verde/TP01/_03AlteracaoAleatoria/Program.cs
+++ Verde/TP01/_03AlteracaoAleatoria/Program.cs
@@ -3,7 +3,31 @@ using System;
 namespace _03AlteracaoAleatoria {
 	class Program {
 		static void Main(string[] args) {
+			string word = "";
 
+			Console.WriteLine("valores: m e d");
+
+			do {
+				word = Console.ReadLine();
+				if (word.ToUpper().Equals("FIM")) 
+					continue;
+
+                string resp = AlteracaoAleatorio(word);
+                Console.WriteLine(resp);
+
+			} while(!word.ToUpper().Equals("FIM"));
 		}
+
+        public static string AlteracaoAleatorio(string word) {
+			Random gerador = new Random (4);
+			char value1 = (char) ('a' + (Math.Abs(gerador.Next()) % 26));
+			char value2 = (char) ('a' + (Math.Abs(gerador.Next()) % 26));
+
+            string newWord = "";
+            for(int i=0; i<word.Length; i++)
+                newWord += word[i] == value1 ? value2 : word[i];
+            
+            return newWord;
+        }
 	}
 }
--- Verde/TP01/_04Is/Program.cs
+++ Verde/TP01/_04Is/Program.cs
@@ -2,8 +2,27 @@ using System;
 
 namespace _04Is {
 	class Program {
+		const char[] vogais = {'a', 'e', 'i', 'o', 'u'};
 		static void Main(string[] args) {
+            string word = "";
 
+			do {
+				word = Console.ReadLine();
+				if (word.ToUpper().Equals("FIM")) 
+					continue;
+
+				Boolean resp = Is(word);
+				Console.WriteLine(word ? "SIM" : "NAO")
+
+			} while(!word.ToUpper().Equals("FIM"));
+		}
+		
+		public static Boolean Is(string word) {
+			for (wint i=0; i<word.Length; i++){
+				if (!word[i].contains(vogais))
+					return false;
+			}
+			return true;
 		}
 	}
 }
