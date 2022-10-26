9a6fd1c1edce5acbafe507f203ea3e7eb708432a
JoaoPedroBotelho <joaopedro@saudeagora.com.vc>
2022-10-26 18:05:04 -0300

    TP02 - 01

---
+++ Verde/TP02/_01Classe/Program.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections;
+
+namespace _01Classe {
+  // Classe Jogadores em C#: Crie uma classe Jogadores. Sua classe terá os atributos Nome (string), Foto (string), Nascimento (Data), Id(int) e Times(Array de inteiros). Ela terá também os métodos imprimir e ler. O método imprimir mostra a string ’id nome nascimento foto (times)’, contendo todos os atributos da classe. O método ler deve efetuar a leitura dos atributos de um registro.
+  // A entrada padrão é composta por várias linhas e cada uma contém dados do jogador que devem  ser tratados e armazenados em objetos da classe Jogador. A última linha da entrada contém FIM. A saída padrão também contém várias linhas, uma para cada registro contido na entrada padrão, conforme o exemplo abaixo:
+	class Program {
+		static void Main(string[] args) {
+      Jogador jogador = new Jogador();
+      jogador.Ler();
+      jogador.Imprimir();
+    }
+	}
+
+  class Jogador {
+    private int id;
+    private string nome;
+    private string foto;
+    private DateTime nascimento;
+    private int[] times;
+
+    public Jogador() {
+
+    }
+
+    public int GetId() {
+      return id;
+    }
+
+    public void SetId(int id) {
+      this.id = id;
+    }
+
+    public string GetNome() {
+      return nome;
+    }
+
+    public void SetNome(string nome) {
+      this.nome = nome;
+    }
+
+    public string GetFoto() {
+      return foto;
+    }
+
+    public void SetFoto(string foto) {
+      this.foto = foto;
+    }
+
+    public DateTime GetNascimento() {
+      return nascimento;
+    }
+
+    public void SetNascimento(DateTime nascimento) {
+      this.nascimento = nascimento;
+    }
+
+    public int[] GetTimes() {
+      return times;
+    }
+
+    public void SetTimes(int[] times) {
+      this.times = times;
+    }
+
+    public void Ler() {
+      string[] resp = Console.ReadLine().Split('[');
+      string[] dados = resp[0].Split(',');
+
+      SetId(Convert.ToInt32(dados[0]));
+      SetNome(dados[1]);
+      SetFoto(dados[2]);
+
+      string[] data = dados[3].Split('/');
+      SetNascimento(new DateTime(Convert.ToInt32(data[2]), Convert.ToInt32(data[1]), Convert.ToInt32(data[0])));
+
+      string[] timesDigitados = resp[1].Substring(0, resp[1].Length-2).Split(',');
+
+      int[] tim = new int[timesDigitados.Length];
+
+      for(int i=0; i<tim.Length; i++) {
+        tim[i] = Convert.ToInt32(timesDigitados[i]);
+      }
+
+      SetTimes(tim);
+    }
+
+    public void Imprimir() {
+      Console.Write("{0} {1} {2} {3} (", GetId(), GetNome(), GetNascimento().ToString("dd/MM/yyyy"), GetFoto());
+
+      int[] t = GetTimes();
+
+      for(int i=0; i<t.Length; i++) {
+        Console.Write("{0}{1}", t[i], (i == t.Length-1 ? ")" : ", "));
+      }
+    }
+  }
+}
